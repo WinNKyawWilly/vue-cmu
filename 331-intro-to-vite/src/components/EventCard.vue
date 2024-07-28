8aaf11ea236a4409f45b178ef357386bf0d76e43
WinNKyawWilly <winnaingkyaw_win@cmu.ac.th>
2024-07-28 23:07:33 +0700

    lab-2 on 8

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -1,8 +1,39 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 
+const event = ref({
+  id: 5928101,
+  category: 'Animal Welfare',
+  title: 'Cat Adoption Day',
+  description: 'Find your new feline friend at this event',
+  location: '123 Main St, Springfield, IL',
+  date: 'January,28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organizer: 'Kat Laydee'
+})
 </script>
 
 <template>
-  <div class="greetings"></div>
+  <div class="event-class">
+    <div class="event-class">
+      <h2> {{ event.title }}</h2>
+      <span>@{{ event.time }} on {{ event.date }}</span>
+    </div>
+  </div>
 </template>
 
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 10px;
+}
+
+.event-card:hover {
+  transform: scale(1.05);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
--- 331-intro-to-vite/src/views/HomeView.vue
+++ 331-intro-to-vite/src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-
+import EventCard from '@/components/EventCard.vue'
 </script>
 
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1>Upcoming Events</h1>
+    
+    <EventCard />
+  </div>
 </template>
