6be9884ae245ab97a99a2206fe4d71ac146cdee1
WinNKyawWilly <winnaingkyaw_win@cmu.ac.th>
2024-08-06 21:15:28 +0700

    latest

---
+++ 331-intro-to-vite/src/views/EventDetailView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import type { Event } from '@/types'
+import EventService from '@/services/EventService'
+
+const event = ref<Event | null>(null)
+const id = ref<number>(5928101)
+
+onMounted(() => {
+    EventService.getEvent(id.value)
+        .then((response) => {
+            event.value = response.data
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+
+})
+</script>
+
+<template>
+    <div v-if="event">
+        <h1>{{ event.title }}</h1>
+        <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+        <p>{{ event.description }}</p>
+    </div>
+</template>
